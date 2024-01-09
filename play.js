c217a89813d4a4e11aee406e44f42c0a9ea937c3
h0lm3 <patrick.holmes@heiltsuk.ca>
2024-01-09 13:51:05 -0800

    play.js

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
-const { connect } = require('./client'); // Import the connect function from client.js
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput); // Register the event listener for stdin
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // Check for the Ctrl + C input to terminate the game
-  if (key === '\u0003') {
-    process.exit();
-  }
-  // Additional key handling logic can be added here
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect(); // Invoke the connect function to establish the connection
+connect();
 
-setupInput(); // Call setupInput
+setupInput();
